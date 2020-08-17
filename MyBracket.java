d21edc34e8e673c19360c254294d7befaade8a9c
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2020-08-17 11:35:22 -0400

    Implement grabbing entrants from challonge

--- MyBracket.java
+++ MyBracket.java
@@ -2,10 +2,14 @@ import MyUtils.*;
 
 class MyBracket
 {
-    public static void main(String [] args)
-    {
+    public static void main(String [] args) {
         // Grab entrants
-        String[] entrants = ReadFile.read_file("SampleData/sample_entrants.txt");
+        //String[] entrants = ReadFile.read_file("SampleData/sample_entrants.txt");
+        String [] entrants = GetEntrants.grab_entrants(args[0]);
+        // Check if entrants were pulled correctly
+        if (entrants.length == 0) {
+            System.exit(1);
+        }
         // Grab Rankings
         String[] rankings = ReadFile.read_file("SampleData/sample_rankings.txt");
         // Convert string rankings to integers
