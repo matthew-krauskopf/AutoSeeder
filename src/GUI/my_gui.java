144f198e0e9be2fb7f27219a192f77da1aa7572e
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2020-09-03 16:42:29 -0400

    Display winner's and loser's matches in window

--- src/GUI/my_gui.java
+++ src/GUI/my_gui.java
@@ -10,6 +10,8 @@ import DBase.DBManager;
 public class my_gui {
 
     static Font font = new Font("Acumin", 0, 16);
+    static int set_gap = 75;
+    static int x_edge = 10;
 
     public static void main(String args[]) {
         main_menu();
@@ -58,9 +60,11 @@ public class my_gui {
     }
 
     public static void seeding_window(String[] entrants, Set[] sets) {
+        // TODO REFACTOR!!!!!
         JFrame window = new JFrame("Suggested Seeding");
         window.setSize(1500,750);
 
+        int y_offset = 0;
         // Create Entrants List
         DefaultListModel<String> l1 = new DefaultListModel<>();
         for (int i = 0; i < entrants.length; i++) {
@@ -71,9 +75,6 @@ public class my_gui {
         // Set font
         list.setFont(font);
 
-        //JLabel label = new JLabel("Enter Challonge link");
-        //label.setBounds(0,0,200,30);
-
         JPanel match_panel = new JPanel(null);
         //match_panel.setLayout(null);
         match_panel.setBounds(0, 0, (int)(window.getWidth()*.125), window.getHeight()-40);
@@ -83,25 +84,58 @@ public class my_gui {
         int sq_entrants = (sets.length+3)/2;
         int round = 1;
         int tot = 0;
-        JLabel [] round_labels = new JLabel[5];
+        JLabel [] w_round_labels = new JLabel[(int) (Math.log10(sq_entrants)/Math.log10(2))];
         JSplitPane [] set_panes = new JSplitPane[sets.length];
         int set_count = 0;
+        int max_win_rs = 0;
         while (tot < sq_entrants-1) {
             //System.out.println("\n" + sq_entrants + " WR " + round);
-            round_labels[round-1] = new JLabel("Winner's Round " + round);
-            round_labels[round-1].setBounds(200*(round-1),0,200,30);
-            round_labels[round-1].setAlignmentX(Component.LEFT_ALIGNMENT);
-            match_panel.add(round_labels[round-1]);
+            w_round_labels[round-1] = new JLabel("Winner's Round " + round);
+            w_round_labels[round-1].setBounds(200*(round-1)+x_edge,0,200,30);
+            w_round_labels[round-1].setAlignmentX(Component.LEFT_ALIGNMENT);
+            match_panel.add(w_round_labels[round-1]);
             int end = (sq_entrants-tot)/2;
             int skipped = 0;
             for (int cur = 0; cur < end ; cur++) {
                 if (!sets[tot].l_player.equals("Bye")) {
-                    JLabel h_seed = new JLabel(sets[tot].h_seed + ": " + sets[tot].h_player);
-                    JLabel l_seed = new JLabel(sets[tot].l_seed + ": " + sets[tot].l_player);
+                    JLabel h_seed = new JLabel(String.format("%3d:   %s",sets[tot].h_seed, sets[tot].h_player));
+                    JLabel l_seed = new JLabel(String.format("%3d:   %s",sets[tot].l_seed, sets[tot].l_player));
                     set_panes[set_count] = new JSplitPane(JSplitPane.VERTICAL_SPLIT, h_seed, l_seed);
-                    set_panes[set_count].setBounds(200*(round-1),100*(cur+1-skipped)-50, 100, 50);
+                    set_panes[set_count].setBounds(200*(round-1)+x_edge,set_gap*(cur+1-skipped)-50, 150, 50);
                     match_panel.add(set_panes[set_count]);
-                    System.out.println(skipped + sets[tot].h_player + " vs " + sets[tot].l_player);
+                    set_count++;
+                }
+                else {
+                    skipped++;
+                }
+                tot++;
+            }
+            max_win_rs = (end-skipped > max_win_rs ? end-skipped : max_win_rs);
+            round++;
+        }
+
+        // Pack loser matches
+        round = 1;
+        // Just accept it. It works
+        JLabel [] l_round_labels = new JLabel[2 * ( (int)(Math.log10(sq_entrants)/Math.log10(2)) - 1)];
+        int end = sq_entrants/4;
+        while(tot < sets.length) {
+            l_round_labels[round-1] = new JLabel("Loser's Round " + round);
+            l_round_labels[round-1].setBounds(200*(round-1)+x_edge,set_gap*(max_win_rs)+50,200,30);
+            l_round_labels[round-1].setAlignmentX(Component.LEFT_ALIGNMENT);
+            match_panel.add(l_round_labels[round-1]);
+            int skipped = 0;
+            for (int cur = 0; cur < end ; cur++) {
+                if (!sets[tot].l_player.equals("Bye")) {
+                    JLabel h_seed = new JLabel(String.format("%3d:   %s",sets[tot].h_seed, sets[tot].h_player));
+                    JLabel l_seed = new JLabel(String.format("%3d:   %s",sets[tot].l_seed, sets[tot].l_player));
+                    set_panes[set_count] = new JSplitPane(JSplitPane.VERTICAL_SPLIT, h_seed, l_seed);
+                    int x_pos = 200*(round-1)+x_edge;
+                    int y_pos = set_gap*(max_win_rs+cur+1-skipped);
+                    // Used to know how large to size window
+                    y_offset = (y_pos > y_offset ? y_pos : y_offset);
+                    set_panes[set_count].setBounds(x_pos, y_pos, 150, 50);
+                    match_panel.add(set_panes[set_count]);
                     set_count++;
                 }
                 else {
@@ -110,26 +144,16 @@ public class my_gui {
                 tot++;
             }
             round++;
-        }
-
-        // Pack loser matches
-        round = 1;
-        int end = sq_entrants/4;
-        while(tot < sets.length) {
-            System.out.println("\n" + sq_entrants + " LR " + round);
-            for (int cur = 0; cur < end ; cur++) {
-                if (!sets[tot].l_player.equals("Bye")) {
-                    System.out.println(sets[tot].h_player + " vs " + sets[tot].l_player);
-                }
-                tot++;
-            }
-            round++;
             if (round % 2 == 1) end /= 2;
         }
 
+        match_panel.setMinimumSize(new Dimension(window.getWidth()-(int)(window.getWidth()*.125)-20, window.getHeight()-40));
+        match_panel.setPreferredSize(new Dimension(200*(round-1), y_offset+60));
+
         JScrollPane matchup_view = new JScrollPane(match_panel);
-        matchup_view.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        matchup_view.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        matchup_view.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        matchup_view.getVerticalScrollBar().setUnitIncrement(16);
+        matchup_view.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         matchup_view.setBounds( (int)(window.getWidth()*.125), 0, window.getWidth()-(int)(window.getWidth()*.125)-20, window.getHeight()-40);
 
         // Attach list of entrants to scroll pane
