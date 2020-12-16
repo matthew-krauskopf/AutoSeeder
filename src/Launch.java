f5d81bae62d42f3e9a35bc73e5af4606731825bc
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2020-12-16 10:53:21 -0500

    Update manifest file, remove "offline" mode for program

--- src/Launch.java
+++ src/Launch.java
@@ -9,29 +9,25 @@ class Launch
     static SU_GUI gui = new SU_GUI();
 
     public static void main(String [] args) throws InterruptedException {
-        if ((args.length > 0) && (args[0].equals("offline"))) {
-            gui.launch();
-        } else {
-            if (!DBase.DBManager.startup()) {
-                System.out.println("Error! Failed to start database...");
-                System.exit(1);
+        if (!DBase.DBManager.startup()) {
+            System.out.println("Error! Failed to start database...");
+            System.exit(1);
+        }
+        // Add graceful shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run()
+            {
+                DBase.DBManager.shutdown();
             }
-            // Add graceful shutdown
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                @Override
-                public void run()
-                {
-                    DBase.DBManager.shutdown();
-                }
-            });
+        });
 
-            // Boot-up Database at launch
-            if (DBase.DBManager.bootUp()) {
-                gui.launch();
-            }
-            else {
-                System.out.println("Error! Could not establish database connection...");
-            }
+        // Boot-up Database at launch
+        if (DBase.DBManager.bootUp()) {
+            gui.launch();
+        }
+        else {
+            System.out.println("Error! Could not establish database connection...");
         }
     }
 }
